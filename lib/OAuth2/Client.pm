7e2b0afd05fcde4c26c4ad7e250216c8bd5bcc91
Keedi Kim <keedi.k@gmail.com>
2013-08-16 17:51:24 +0900

    Combine Traits::Client and remove it
    현재는 OAuth::Client::Traits::Client 역할 모듈을 사용하는 곳은
    OAuth::Client 메인 모듈 밖에 없는 것으로 보입니다. 이 경우 역할
    모듈을 따로 나눌 필요는 없을 것 같습니다. 복잡도 감소를 위해
    메인 모듈로 합치고 추후 코드 중복이 발생하는 경우 다시 역할 모듈로
    나누면 될 것 같습니다.

--- lib/OAuth2/Client.pm
+++ lib/OAuth2/Client.pm
@@ -2,13 +2,48 @@ package OAuth2::Client;
 
 use Moose;
 
+use HTTP::Cookies;
 use HTTP::Request;
 use JSON::XS;
+use LWP::UserAgent;
+use MIME::Base64 'encode_base64';
 use Time::Piece;
 use URI::QueryParam;
 use URI;
 
-with 'OAuth2::Traits::Client';
+has 'ua' => (
+    is  => 'ro',
+    isa => 'LWP::UserAgent',
+    default => sub {
+        my $ua = LWP::UserAgent->new(
+            agent        => 'OAuth2::Client',
+            cookie_jar   => HTTP::Cookies->new,
+            max_redirect => 0,
+        );
+
+        $ua->add_handler(
+            'request_prepare' => sub {
+                my ($req, $ua, $h) = @_;
+                if ($ENV{DEBUG}) {
+                    my $req_string = $req->as_string =~ s{\n}{\n> }mgr;
+                    print "> $req_string";
+                }
+            }
+        );
+
+        $ua->add_handler(
+            'response_done' => sub {
+                my ($res, $ua, $h) = @_;
+                if ($ENV{DEBUG}) {
+                    my $res_string = $res->as_string =~ s{\n}{\n< }mgr;
+                    print "< $res_string";
+                }
+            }
+        );
+
+        return $ua;
+    },
+);
 
 our @GRANT_TYPES = (
     'Authorization Code',
@@ -34,6 +69,13 @@ has 'expires' => (
     is  => 'rw',
     isa => 'Int',
 );
+
+sub basic_credentials {
+    my ($self, $username, $password) = @_;
+
+    return '' unless $username && $password;
+    return "Basic " . encode_base64("$username:$password", '');
+}
 
 sub authorization_request {
     my ($self, %args) = @_;
