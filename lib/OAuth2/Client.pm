7f2de6d27c5e2841806d43c8454eb0e337b7fafb
Keedi Kim <keedi.k@gmail.com>
2013-08-16 18:23:14 +0900

    Remove debug log handler for refactoring
    디버깅 로그 핸들러가 있으면 유용하나 일단 지금은 리팩터링을
    위해 디버깅용 코드를 제거하도록 합니다.

--- lib/OAuth2/Client.pm
+++ lib/OAuth2/Client.pm
@@ -17,33 +17,11 @@ has ua => (
     is      => 'ro',
     isa     => sub { die "ua must be LWP::UserAgent" unless ref($_[0]) eq 'LWP::UserAgent' },
     default => sub {
-        my $ua = LWP::UserAgent->new(
+        LWP::UserAgent->new(
             agent        => 'OAuth2::Client',
             cookie_jar   => HTTP::Cookies->new,
             max_redirect => 0,
         );
-
-        $ua->add_handler(
-            'request_prepare' => sub {
-                my ($req, $ua, $h) = @_;
-                if ($ENV{DEBUG}) {
-                    my $req_string = $req->as_string =~ s{\n}{\n> }mgr;
-                    print "> $req_string";
-                }
-            }
-        );
-
-        $ua->add_handler(
-            'response_done' => sub {
-                my ($res, $ua, $h) = @_;
-                if ($ENV{DEBUG}) {
-                    my $res_string = $res->as_string =~ s{\n}{\n< }mgr;
-                    print "< $res_string";
-                }
-            }
-        );
-
-        return $ua;
     },
 );
 
